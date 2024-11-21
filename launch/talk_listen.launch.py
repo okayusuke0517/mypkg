908a157987e3c5ccc27a841e15dc630a831d1b46
okayusuke2024 <s23C1031KK@s.chibakoudai.jp>
2024-11-21 16:56:34 +0900

    Initial commit with ROS2 package files

---
+++ launch/talk_listen.launch.py
@@ -0,0 +1,35 @@
+import launch.actions
+import launch.substitutions
+import launch
+import launch_ros.actions
+
+
+def generate_launch_description():
+    # talker ノードを定義
+    talker = launch_ros.actions.Node(
+        package='mypkg',       # パッケージ名
+        executable='talker',   # 実行するノード
+    )
+
+    # listener ノードを定義
+    listener = launch_ros.actions.Node(
+        package='mypkg',
+        executable='listener',
+        output='screen',       # ログを端末に出力
+    )
+
+    # LaunchDescription を返す
+    return launch.LaunchDescription([
+        talker,
+        listener,
+    ])
+
+
+  
+
+
+
+
+
+
+
