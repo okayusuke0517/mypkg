4430d58029221673a8cea0fe8e4ec1487119a754
okayusuke2024 <s23C1031KK@s.chibakoudai.jp>
2024-11-21 16:56:34 +0900

    Initial commit with ROS2 package files

---
+++ mypkg/talker.py
@@ -0,0 +1,19 @@
+import rclpy
+from rclpy.node import Node
+from person_msgs.srv import Query
+
+rclpy.init()
+node = Node("talker")
+
+def cb(request, response):
+    if request.name == "岡湧育":
+        response.age = 20
+    else:
+        response.age = 255
+
+    return response
+
+
+def main():
+    srv = node.create_service(Query, "query", cb) #サービスの作成     
+    rclpy.spin(node)
