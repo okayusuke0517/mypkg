cf54c8c6b8f5f2189d9d5bb769025c2795cb90ea
okayusuke2024 <s23C1031KK@s.chibakoudai.jp>
2024-12-29 19:08:49 +0900

    test5

--- test/test.bash
+++ test/test.bash
@@ -6,7 +6,7 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 15 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 20 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
 grep 'Received Count: 5 あっち向いてホイ'
