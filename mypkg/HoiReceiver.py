ef7204dc2b85cf05114c87fb63958ec4397f5e88
okayusuke2024 <s23C1031KK@s.chibakoudai.jp>
2024-12-29 20:45:11 +0900

    LICENSE

--- mypkg/HoiReceiver.py
+++ mypkg/HoiReceiver.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+#SPDX-FileCopyrightText: 2024 Yusuke Oka
+#SPDX-License-Identifier: BSD-3-Clause
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16, String
--- test/test.bash
+++ test/test.bash
@@ -1,12 +1,13 @@
-#!/bin/bash
-
+#!/bin/bash -xv
+#SPDX-FileCopyrightText: 2024 Yusuke Oka
+#SPDX-License-Identifier: BSD-3-Clause
 dir=~
 [ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 20 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 15 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
 grep 'Received Count: 5 あっち向いてホイ'
