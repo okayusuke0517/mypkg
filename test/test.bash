2b59c890c047c14bf43407ee88836f6c057997d3
okayusuke2024 <s23C1031KK@s.chibakoudai.jp>
2024-12-31 00:28:49 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -11,5 +11,4 @@ timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
 grep -E "Round: 5, Direction: (上|下|左|右), The day we are playing: (Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)"
-#cat /tmp/mypkg.log |
-#grep -E "Round: 10, Direction: (上|下|左|右), The day we are playing: (Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)"
+
