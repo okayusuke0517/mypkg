ad38384472f5aadbb16505b9b72966d189cea88f
okayusuke2024 <s23C1031KK@s.chibakoudai.jp>
2024-12-31 00:06:05 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -7,9 +7,9 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 15 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
 grep -E "Round: 5, Direction: (上|下|左|右), The day we are playing: (Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)"
-cat /tmp/mypkg.log |
-grep -E "Round: 10, Direction: (上|下|左|右), The day we are playing: (Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)"
+#cat /tmp/mypkg.log |
+#grep -E "Round: 10, Direction: (上|下|左|右), The day we are playing: (Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)"
