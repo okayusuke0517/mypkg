91ad6b93e253bf9509568d4321b1d6a9a506aada
okayusuke2024 <s23C1031KK@s.chibakoudai.jp>
2024-12-31 01:48:45 +0900

    test

--- mypkg/AchiMuiteHoiPublisher.py
+++ mypkg/AchiMuiteHoiPublisher.py
@@ -12,13 +12,13 @@ class AchiMuiteHoiPublisher(Node):
         self.pub_game_status = self.create_publisher(String, "game_status", 10)
 
         # タイマーの間隔を１．０秒に設定
-        self.create_timer(0.5, self.publish_status)
+        self.create_timer(1.0, self.publish_status)
         self.n = 0
 
     def publish_status(self):
             direction = random.choice(["上", "下", "左", "右"])
             current_day = datetime.now().strftime('%A')
-            message = f"Round: {self.n}, Direction: {direction}, The day we are playing: {current_day}"            
+            message = f"Round:{self.n} Direction:{direction} The day we are playing:{current_day}"            
             status_msg = String()
             status_msg.data = message
             self.pub_game_status.publish(status_msg)
--- test/test.bash
+++ test/test.bash
@@ -9,11 +9,8 @@ colcon build
 source $dir/.bashrc
 timeout 20 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-echo "==== Checking Direction ===="
-cat /tmp/mypkg.log | grep -E "Direction: (上|下|左|右)"
+cat /tmp/mypkg.log | grep -E "Direction:(上|下|左|右)"
 
-echo "==== Checking Round ===="
-cat /tmp/mypkg.log | grep 'Round: 5'
+#cat /tmp/mypkg.log | grep 'Round:5'
 
-echo "==== Checking Day ===="
-cat /tmp/mypkg.log | grep -E "The day we are playing: (Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)"
+#cat /tmp/mypkg.log | grep -E "The day we are playing:(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)"
