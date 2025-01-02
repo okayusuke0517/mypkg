ef7f76fcd732dd6d4862f6b01b3ec15d3d5cf7a6
okayusuke2024 <s23C1031KK@s.chibakoudai.jp>
2025-01-02 17:43:36 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -16,6 +16,8 @@ timeout 10 ros2 run mypkg hoistatuspublisher > /tmp/mypkg.log &
 
 ros2 topic echo /game_status > /tmp/game_status.log &
 
+sleep 5
+
 cat /tmp/game_status.log | grep -E "Direction: (上|下|左|右)"
 
 cat /tmp/game_status.log | grep 'Round: 5'
